6b4dee13fb0bef1eac699cd43e7d42b2fda59a06
jordanlira <jordanjr.92@gmail.com>
2019-01-08 11:19:41 -0300

    Add CustomCollections rows views Fix project to use AndroidX

---
+++ app/src/main/java/com/araujo/jordan/shopifymobilesummerchallenge2019/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.araujo.jordan.shopifymobilesummerchallenge2019
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
